cd7b257a62938fc0aeb6940a2a79861a7e4c8642
Anton Sayutin <72465013+Sayut61@users.noreply.github.com>
2023-06-01 19:14:11 +0300

    feature/0.0.1: добавил три фаргмента, зависимости навигации, граф навигации.

--- app/src/main/java/com/example/testappmangofzco/AuthFragment.kt
+++ app/src/main/java/com/example/testappmangofzco/AuthFragment.kt
@@ -5,19 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.testappmangofzco.databinding.FragmentProfileBinding
+import com.example.testappmangofzco.databinding.FragmentAuthBinding
 
-class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentProfileBinding? = null
+class AuthFragment : Fragment() {
+
+    private var _binding: FragmentAuthBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
