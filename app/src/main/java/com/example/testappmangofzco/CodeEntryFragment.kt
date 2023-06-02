163d43c8fef3c32e737c62046057523dbcd24ece
Anton Sayutin <72465013+Sayut61@users.noreply.github.com>
2023-06-02 16:32:55 +0300

    feature/0.0.6: Добавил функцию обработки ввода кода из смс на экране верификации

--- app/src/main/java/com/example/testappmangofzco/CodeEntryFragment.kt
+++ app/src/main/java/com/example/testappmangofzco/CodeEntryFragment.kt
@@ -1,18 +1,111 @@
 package com.example.testappmangofzco
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.testappmangofzco.databinding.FragmentCodeEntryBinding
 
 class CodeEntryFragment : Fragment() {
 
+    private var _binding: FragmentCodeEntryBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_code_entry, container, false)
+        _binding = FragmentCodeEntryBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupVerifyCodeInputs()
+    }
+
+
+    private fun setupVerifyCodeInputs() {
+        binding.apply {
+            inputCode1.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    inputCode2.requestFocus()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+            inputCode2.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    inputCode3.requestFocus()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+            inputCode3.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    inputCode4.requestFocus()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+            inputCode4.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    inputCode5.requestFocus()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+            inputCode5.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    inputCode6.requestFocus()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+        }
+    }
 }
