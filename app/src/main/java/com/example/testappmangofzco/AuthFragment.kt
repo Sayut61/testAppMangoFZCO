1da63b4b04ba8d68010d08cfc7b76d35d64bd07f
Anton Sayutin <72465013+Sayut61@users.noreply.github.com>
2023-06-03 12:13:23 +0300

    feature/0.0.6: Добавил слушателя на клик отправки номера, добавил анимации переходов между экранами

--- app/src/main/java/com/example/testappmangofzco/AuthFragment.kt
+++ app/src/main/java/com/example/testappmangofzco/AuthFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.testappmangofzco.databinding.FragmentAuthBinding
@@ -25,12 +26,30 @@ class AuthFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         goToRegistrationScreen()
+        onClickSendBtn()
     }
 
     private fun goToRegistrationScreen() {
         binding.registrationTv.setOnClickListener {
             AuthFragmentDirections.actionAuthFragmentToRegistrationFragment().apply {
                 findNavController().navigate(this)
+            }
+        }
+    }
+
+    private fun onClickSendBtn() {
+        binding.sendBtn.setOnClickListener {
+            if (binding.inputNumber.text.toList().size < 10) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.enter_mobile),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                AuthFragmentDirections.actionAuthFragmentToCodeEntryFragment(binding.inputNumber.text.toString())
+                    .apply {
+                        findNavController().navigate(this)
+                    }
             }
         }
     }
